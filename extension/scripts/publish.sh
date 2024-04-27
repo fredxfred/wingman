8e70877485c66d89066799353fde5733eea9ab5f
Fred Weitendorf <37577626+fredxfred@users.noreply.github.com>
2024-04-27 15:17:40 +0000

    try to get extension detail page working

--- extension/scripts/publish.sh
+++ extension/scripts/publish.sh
@@ -3,8 +3,8 @@ set -e
 npm --no-git-tag-version version patch
 npm run build:browser
 npm install -g @vscode/vsce
-vsce package && vsce login mplodeai
-vsce publish
+vsce package --no-dependencies
+vsce login mplodeai && vsce publish --no-dependencies
 printf 'Supply Eclipse OpenVSX token: '
 read vsx
 npx ovsx publish -p $vsx
